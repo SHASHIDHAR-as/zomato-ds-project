453f8263290584878a940133096441bbb1745b50
Suchith Kumar G M <suchithkumargm@gmail.com>
2024-02-21 14:34:00 +0530

    create new order done

--- server/controllers/orders/orderController.js
+++ server/controllers/orders/orderController.js
@@ -3,41 +3,45 @@ import { validationResult } from 'express-validator';
 import Order from '../../models/Order.js';
 
 export const getCustomerOrders = async (req, res) => {
-  try {
-    const customerOrders = await Order.find({ customer_id: req.params.customer_id });
-    res.json(customerOrders);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
+    try {
+        const customerOrders = await Order.find({ customer_id: req.params.customer_id });
+        res.json(customerOrders);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 };
 
-export const lodgeNewTrainComplaint = async (req, res) => {
-  try {
-    // Get all details from the body of the request
-    const { trainNumber, pnrNumber, type, subtype, timestamp, description } = req.body;
-    
-    // If there are errors, return a bad request and the errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+export const createNewOrder = async (req, res) => {
+    try {
+
+        let order_id;
+        do {
+            order_id = Math.floor(100000 + Math.random() * 999999); // Generate a random 6-digit number
+        } while (await Order.findOne({ order_id })); // Check if the generated order ID already exists in the database
+
+        // Get all details from the body of the request
+        const { customer_id, restaurant_id, name, location, date } = req.body;
+
+        // If there are errors, return a bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const order = new Order({
+            order_id,
+            customer_id,
+            restaurant_id,
+            name,
+            location,
+            date
+        });
+
+        const savedOrder = await order.save();
+        res.json(savedOrder);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
     }
-
-    // Create an object of train complaint
-    const trainComplaint = new TrainComplaint({
-      user: req.user.id,
-      trainNumber,
-      pnrNumber,
-      type,
-      subtype,
-      timestamp,
-      description,
-    });
-
-    const savedComplaint = await trainComplaint.save();
-    res.json(savedComplaint);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
 };
--- server/routes/orders/orders.js
+++ server/routes/orders/orders.js
@@ -2,14 +2,13 @@ import express from 'express';
 import { body } from 'express-validator';
 
 import {
+  createNewOrder,
   getCustomerOrders
 } from '../../controllers/orders/orderController.js';
 
 const router = express.Router();
 
-// ROUTE 1: Get All the restaurants using: GET "/restaurants/traincomplaints/getallcomplaints". manager Login required
-
-// ROUTE 2: Get all customers orders using: GET "/orders/customer_id".
+router.post('/createNewOrder',createNewOrder)
 router.get('/getCustomerOrders/:customer_id', getCustomerOrders);
 
 export default router;
